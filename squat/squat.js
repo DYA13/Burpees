3c94000ae9d9d20a978748fd7acf0d10b0e20a50
Yelena <88336329+DYA13@users.noreply.github.com>
2022-11-07 11:57:58 +0100

    first commit

---
+++ squat/squat.js
@@ -0,0 +1,70 @@
+const video=document.querySelector("#squatVid");  
+const myAudio=document.querySelector("#musicPlay");
+function playVideo(){
+ video.play(); 
+ myAudio.play()
+}
+function stopVideo(){
+    video.pause();
+    myAudio.pause();
+    video.currentTime = 0;
+}
+
+
+
+let timerID
+const buttonPlay = document.querySelector('#squatStart'); 
+buttonPlay.addEventListener("click", function() {
+    timerID = setInterval(timerS, 1000);
+    playVideo();
+   
+})
+
+const buttonStop = document.querySelector('#squatPause'); 
+buttonStop.addEventListener("click", function() {
+    stopTimer()
+   stopVideo()
+});
+
+const timer = 3;
+let amountTime = timer * 60;
+
+function timerS() {
+    const countdown = document.querySelector('#squatCountdown');
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+
+    if(amountTime < 0) {
+       amountTime = 0;
+       stopVideo()
+       countdown.innerHTML = "Fatto!";
+       setTimeout(resetTimer, 5000);
+    }
+
+}
+
+function stopTimer() {
+         clearInterval(timerID);
+     
+      }
+
+
+    function resetTimer() {
+        location.reload();
+    }
+
+
+
+
+
+
+
+
+
